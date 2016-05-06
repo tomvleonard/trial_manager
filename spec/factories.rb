4ed719290b6b22369f7f5daa0b0c0291d8f24611
Tom Leonard <tvleonard@gmail.com>
2016-05-06 15:07:06 -0400

    Add spec/factories and modify spec/rails_helper to include factory girl and shoulda-matchers.

---
+++ spec/factories.rb
@@ -0,0 +1,29 @@
+FactoryGirl.define do
+
+  factory :admin do
+    sequence(:first_name) { |n| "Admin#{n}" }
+    sequence(:last_name) { |n| "McAdminheimer#{n}" }
+    sequence(:email) { |n| "admin_mcadminheimer#{n}@gmail.com" }
+    password 'password'
+    password_confirmation 'password'
+  end
+
+  factory :user do
+    sequence(:first_name) { |n| "User#{n}" }
+    sequence(:last_name) { |n| "McUsenheimer#{n}" }
+    sequence(:email) { |n| "user_mcusenheimer#{n}@gmail.com" }
+    password 'password'
+    password_confirmation 'password'
+  end
+
+  factory :organization do
+    sequence(:name) { |n| "Organization Number #{n}" }
+    sequence(:contact_name) { |n| "Orga Mcnization#{n}" }
+    sequence(:contact_email) { |n| "organization_number_#{n}@gmail.com" }
+    contact_phone '617-123-0001'
+    sequence(:street) { |n| "#{n} Main Street" }
+    city 'Exeter'
+    state 'NH'
+    postal_code '09876'
+  end
+end
