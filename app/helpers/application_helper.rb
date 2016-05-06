966c9caa30acbb2a2168d5280b559496632cee8c
Tom Leonard <tvleonard@gmail.com>
2016-05-06 10:49:36 -0400

    Add home/index view and apply bootstrap to views/layouts/application (first pass).

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
+  def title_html(html_data)
+    content_for(:title_html) { html_data.html_safe }
+  end
+
 end
