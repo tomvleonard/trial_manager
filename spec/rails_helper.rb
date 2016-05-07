cad12bbd5a04ff0c6d3c4a831dbcaaaac3cb7e42
Tom Leonard <tvleonard@gmail.com>
2016-05-07 12:26:32 -0400

    Add rails routes url helpers, capyara dsl and warden test helpers config to spec/rails_helper.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,7 +55,13 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Rails.application.routes.url_helpers
+  config.include Capybara::DSL
   config.include FactoryGirl::Syntax::Methods
+  config.include Warden::Test::Helpers
+  config.before :suite do
+    Warden.test_mode!
+  end
 end
 
 Shoulda::Matchers.configure do |config|
