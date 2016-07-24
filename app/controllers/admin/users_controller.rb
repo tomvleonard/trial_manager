b09b75442c30b1e6675597fac481e2f1fa0f8aa7
Tom Leonard <tvleonard@gmail.com>
2016-07-24 13:52:53 -0400

    Add users#show and link to it from users#index.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,4 +4,9 @@ class Admin::UsersController < AdminController
     @user_presenters = User.includes(user_organizations: :organization).order(:last_name, :first_name).inject([]) { |a, u| a << UserPresenter.new(u); a }
   end
 
+  def show
+    user = User.includes(:user_organizations).where(id: params[:id]).first
+    @user_presenter = UserPresenter.new(user)
+  end
+
 end
--- spec/acceptance/admin/users_spec.rb
+++ spec/acceptance/admin/users_spec.rb
@@ -4,6 +4,7 @@ describe 'admin/users' do
   before do
     login_as create(:admin), scope: :admin
     @user = create :user
+    @user_presenter = UserPresenter.new(@user)
     @organization = create :organization
     create :user_organization_administrator, user: @user, organization: @organization
   end
@@ -13,14 +14,64 @@ describe 'admin/users' do
       visit admin_users_path
     end
 
-    it 'should show user first name' do
-      expect(page).to have_content(@user.first_name)
+    it 'should show user full name' do
+      expect(page).to have_content(@user_presenter.full_name)
     end
 
-    it 'should show user last name' do
-      expect(page).to have_content(@user.last_name)
+    it 'should link to user' do
+      expect(page).to have_xpath("//a[@href='#{admin_user_path(@user)}']")
     end
 
+    it 'should show user email' do
+      expect(page).to have_content(@user_presenter.email)
+    end
+
+    it 'should show organizations with membership type' do
+      expect(page).to have_content(@user_presenter.user_organization_presenters.first.organization_name_with_membership)
+    end
+
+    it 'should link to organizations' do
+      expect(page).to have_xpath("//a[@href='#{admin_organization_path(@organization)}']")
+    end
+
+    it 'should show create date' do
+      expect(page).to have_content(@user_presenter.full_name)
+    end
+  end
+
+  describe 'show' do
+    before do
+      @user.update_attributes sign_in_count: 99,
+        current_sign_in_at: Date.today,
+        last_sign_in_at: Date.today - 1.day,
+        current_sign_in_ip: '1.2.3.4',
+        last_sign_in_ip: '5.6.7.8',
+        confirmed_at: Date.today - 2.days,
+        failed_attempts: 11
+      visit admin_user_path(@user)
+    end
+
+    it 'should show user details' do
+      expect(page).to have_content(@user_presenter.full_name)
+      expect(page).to have_content(@user_presenter.email)
+      expect(page).to have_content(@user_presenter.sign_in_count)
+      expect(page).to have_content(@user_presenter.current_sign_in_at)
+      expect(page).to have_content(@user_presenter.last_sign_in_at)
+      expect(page).to have_content(@user_presenter.current_sign_in_ip)
+      expect(page).to have_content(@user_presenter.last_sign_in_ip)
+      expect(page).to have_content(@user_presenter.confirmed?)
+      expect(page).to have_content(@user_presenter.failed_attempts)
+      expect(page).to have_content(@user_presenter.locked?)
+    end
+
+    it 'should show organization names and type of membership' do
+      expect(page).to have_content(@user_presenter.user_organization_presenters.first.organization_name)
+      expect(page).to have_content(@user_presenter.user_organization_presenters.first.membership)
+    end
+
+    it 'should have link to organizations' do
+      expect(page).to have_xpath("//a[@href='#{admin_organization_path(@organization)}']")
+    end
   end
 
 end
