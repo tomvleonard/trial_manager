21bc27589a2de8fdb889d296661efd14d5d2d09a
Tom Leonard <tvleonard@gmail.com>
2016-07-24 12:02:48 -0400

    Add organizations#show page.

--- app/controllers/admin/organizations_controller.rb
+++ app/controllers/admin/organizations_controller.rb
@@ -4,4 +4,8 @@ class Admin::OrganizationsController < AdminController
     @organization_presenters = Organization.includes(user_organizations: :user).order(:name).inject([]) { |a, o| a << OrganizationPresenter.new(o); a }
   end
 
+  def show
+    @organization_presenter = OrganizationPresenter.new(Organization.find(params[:id]))
+  end
+
 end
