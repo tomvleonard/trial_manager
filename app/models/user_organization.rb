e065db0c5f8f2f790ba58528e9473ce3370c0bcf
Tom Leonard <tvleonard@gmail.com>
2016-07-24 12:52:34 -0400

    Add delegations to UserOrganization.

--- app/models/user_organization.rb
+++ app/models/user_organization.rb
@@ -3,4 +3,7 @@ class UserOrganization < ApplicationRecord
   belongs_to :user
   belongs_to :organization
 
+  delegate :name, prefix: true, to: :organization
+  delegate :first_name, :last_name, prefix: true, to: :user
+
 end
--- spec/models/user_organization_spec.rb
+++ spec/models/user_organization_spec.rb
@@ -6,4 +6,24 @@ RSpec.describe UserOrganization, type: :model do
     it { should belong_to assoc }
   end
 
+  describe 'delegations' do
+    it '.organization_name' do
+      organization = Organization.new(name: 'This org')
+      user_organization = UserOrganization.new(organization: organization)
+      expect(user_organization.organization_name).to eq 'This org'
+    end
+
+    it '.user_first_name' do
+      user = User.new(first_name: 'First')
+      user_organization = UserOrganization.new(user: user)
+      expect(user_organization.user_first_name).to eq 'First'
+    end
+
+    it '.user_last_name' do
+      user = User.new(last_name: 'Last')
+      user_organization = UserOrganization.new(user: user)
+      expect(user_organization.user_last_name).to eq 'Last'
+    end
+  end
+
 end
