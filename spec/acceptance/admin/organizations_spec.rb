144c5731c92313abcabfff9763b0ed14de7c4167
Tom Leonard <tvleonard@gmail.com>
2016-07-24 12:03:30 -0400

    Add acceptance tests for admin/organizations#show and changes to #index.

--- spec/acceptance/admin/organizations_spec.rb
+++ spec/acceptance/admin/organizations_spec.rb
@@ -18,9 +18,17 @@ describe 'admin/organizations' do
       expect(page).to have_content(@organization_presenter.name)
     end
 
+    it 'should have link to organization show page' do
+      expect(page).to have_xpath("//a[@href='#{admin_organization_path(@organization)}']")
+    end
+
     it 'should show member names with membership type' do
       user_organization_presenter = @organization_presenter.user_organization_presenters.first
       expect(page).to have_content(user_organization_presenter.user_name_with_membership)
+    end
+
+    it 'should have link to members' do
+      expect(page).to have_xpath("//a[@href='#{admin_user_path(@user)}']")
     end
 
     it 'should show street' do
@@ -53,6 +61,32 @@ describe 'admin/organizations' do
 
     it 'should show create date' do
       expect(page).to have_content(@organization_presenter.create_date)
+    end
+  end
+
+  describe 'show' do
+    before do
+      visit admin_organization_path(@organization)
+    end
+
+    it 'should show organization details' do
+      expect(page).to have_content(@organization_presenter.name)
+      expect(page).to have_content(@organization_presenter.street)
+      expect(page).to have_content(@organization_presenter.city)
+      expect(page).to have_content(@organization_presenter.state)
+      expect(page).to have_content(@organization_presenter.postal_code)
+      expect(page).to have_content(@organization_presenter.contact_name)
+      expect(page).to have_content(@organization_presenter.contact_email)
+      expect(page).to have_content(@organization_presenter.contact_phone)
+      expect(page).to have_content(@organization_presenter.create_date)
+    end
+
+    it 'should show member names' do
+      expect(page).to have_content(@organization_presenter.user_organization_presenters.first.user_full_name)
+    end
+
+    it 'should link to members' do
+      expect(page).to have_xpath("//a[@href='#{admin_user_path(@user)}']")
     end
 
   end
