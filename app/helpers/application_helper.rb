86bd2ccffaf0ad950f19565d8a7ed93767e20967
Tom Leonard <tvleonard@gmail.com>
2016-09-11 11:42:46 -0400

    Add link button for add/edit/delete to application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,17 @@ module ApplicationHelper
     content_for(:title_html) { html_data.html_safe }
   end
 
+  def small_add_button(path, text='ADD')
+    link_to text, path, class: 'btn btn-small btn-green'
+  end
+
+  def small_edit_button(path, text='EDIT')
+    link_to text, path, class: 'btn btn-small btn-yellow'
+  end
+
+  def small_delete_button(path, text='DELETE')
+    link_to text, path, class: 'btn btn-small btn-red', method: :delete, data: { confirm: 'Are you sure you want to delete this item?' }
+  end
+
+
 end
